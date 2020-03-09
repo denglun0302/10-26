5adc71b4c5ed3bb5a8917be5a19fd290af460cd1
denglun0302 <2393751439@qq.com>
2020-03-09 19:59:25 +0800

    提交代码

---
+++ work/m.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+ {
+    int i=1;
+     for(i=1;i<=10;i++)
+     {   
+
+          if (5==i)
+          break ;
+          printf("%d",i);
+       }
+   return 0;
+   }
